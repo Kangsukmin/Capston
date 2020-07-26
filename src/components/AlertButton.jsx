f396bc8bf0b75c8b94a60d3a817f610771324752
seokminKang <soos0228@naver.com>
2020-07-26 21:13:31 +0900

    firestore 설정완료, 동기식, 비동기식 설정목표

--- src/components/AlertButton.jsx
+++ src/components/AlertButton.jsx
@@ -4,33 +4,23 @@ import Badge from '@material-ui/core/Badge';
 import { useSelector } from "react-redux";
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { useFirestoreConnect } from "react-redux-firebase";
-import { useFirestore } from "react-redux-firebase";
 
 export default function AlertButton(){
-    const firestore = useFirestore();
-
     useFirestoreConnect({
         collection: 'alerts' ,
         storeAs: "alerts",
     })
     
-    const alerts = useSelector((state) => state.firestore.data);
+    const alerts = useSelector((state) => state.firestore.data.alerts);
+
     const handleOnClick = () => {
-        firestore
-            .collection("alerts")
-            .add({
-                title: 'hello!',
-                isDone: false,
-            })
-            .then((docRef) => {
-                docRef.update({
-                todoID: docRef.id,
-                });
-            });
+        console.log(alerts);
     }
+
+
     return(
         <IconButton color="inherit" onClick={handleOnClick}>
-            <Badge badgeContent={4} color="secondary">
+            <Badge badgeContent={1} color="secondary">
                 <NotificationsIcon />
             </Badge>
         </IconButton>
